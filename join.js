3a837d7f3b9f168f69c9ce531b0288c4ccb1c910
leedahee123 <73813783+leedahee123@users.noreply.github.com>
2021-05-10 16:55:39 +0900

    html

--- join.js
+++ join.js
@@ -18,7 +18,7 @@ window.addEventListener('load', function() {
 			 
 			
 			// 아이디와 비번,성별,취미,생일,나이,이메일,메모 객체 셋팅  
-			var email = document.querySelector('#email');
+			var id = document.querySelector('#id');
 			var pw1 = document.querySelector('#pw1');
 			var pw2 = document.querySelector('#pw2');
 			var name = document.querySelector('#name');
@@ -39,13 +39,13 @@ window.addEventListener('load', function() {
 
 			// 전체 검사
 			// 이메일이 공백이거나 4글자 미만이면
-			if (email.value == '' || email.value.length < 4) {
-				alert('이메일을 4자이상!');
+			if (id.value == '' || id.value.length < 4) {
+				alert('아이디 4자이상');
 				//id포커스
-				email.focus();
+				id.focus();
 			// 비밀번호가 4자리 미만이거나 숫자가 아닐때
 			} else if (pw1.value.length < 4/*  || isNaN(pw1.value) */) {
-				alert('비번 숫자로 4자이상쓰셈');
+				alert('패스워드 4자이상');
 				//비밀번호 포커스
 				pw1.focus();
 			// 비밀번호가 처음입력한 값과 다를시
@@ -59,7 +59,7 @@ window.addEventListener('load', function() {
             // 폰번호가 입력되지 않았을시
             } else if (phone.value == '') {
 				alert('전화번호 공백안됨');
-				email.focus();
+				phone.focus();
 			// 성별이 체크가 안되었을시
 			} else if (genderVal == false) {
 				alert("성별을 입력하세요!");
